413040a4575f4ef09ee8ca47f415ecac68589ecf
GeorgeBinDragon <georgebindragon@gmail.com>
2024-06-24 23:15:53 +0800

    crop pic

--- download_image.py
+++ download_image.py
@@ -1,18 +1,18 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from PIL import Image
 import os
 
-# 获取当前日期
-today = datetime.today().strftime('%Y%m%d')
-year = datetime.today().strftime('%Y')
-month_day = datetime.today().strftime('%m%d')
+# 获取明天的日期
+tomorrow = datetime.now() + timedelta(days=1)
+year = tomorrow.strftime('%Y')
+month_day = tomorrow.strftime('%m%d')
 
 # 图片 URL
 url = f"https://img.owspace.com/Public/uploads/Download/{year}/{month_day}.jpg"
 
 # 保存路径
-filename = f"images/{today}.jpg"
+filename = f"images/{tomorrow.strftime('%Y%m%d')}.jpg"
 output_dir = "Wallpaper"
 output_filename = os.path.join(output_dir, "today.png")
 
@@ -32,21 +32,19 @@ else:
 # 裁剪和压缩图片
 try:
     with Image.open(filename) as img:
-        # 原始尺寸
-        original_width, original_height = img.size
-        print(f"Original size: {original_width}x{original_height}")
-
-        # 裁剪图片，保持居中
-        target_width, target_height = 758, 1024
-        left = (original_width - target_width) / 2
-        top = (original_height - target_height) / 2
-        right = (original_width + target_width) / 2
-        bottom = (original_height + target_height) / 2
-
+        # 剪裁图片
+        left = 60
+        top = 100
+        right = img.width - 60
+        bottom = img.height - 312
         cropped_img = img.crop((left, top, right, bottom))
 
+        # 压缩图片
+        target_width, target_height = 758, 1024
+        resized_img = cropped_img.resize((target_width, target_height), Image.ANTIALIAS)
+
         # 保存为 PNG
-        cropped_img.save(output_filename, "PNG")
+        resized_img.save(output_filename, "PNG")
         print(f"Processed image saved as {output_filename}")
 except Exception as e:
     print(f"Failed to process image: {e}")
